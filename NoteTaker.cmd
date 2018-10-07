c3de906297b9488d48b21745d7f9c071dfd55402
Douglas Bolster <doug.bolster@gmail.com>
2018-10-06 22:36:45 -0400

    -created batch and new jar

--- NoteTaker.cmd
+++ NoteTaker.cmd
@@ -4,23 +4,6 @@ SETLOCAL ENABLEEXTENSIONS
 set arg1=%1
 set arg2=%2
 
-if arg1="" GOTO :ONEARG
-if arg2="" GOTO :TWOARG
-
-:ONEARG
-IF %arg1%=="-h" (
-java -jar NoteTaker -h 
-)
-if %arg1%=="-nn" (
-java -jar NoteTaker -lc
-)
-
-:TWOARG 
-if %arg1%=="-ac" (
-java -jar NoteTaker -ac %arg2%
-)
-if %arg1%=="-nn" (
-java -jar NoteTaker -nn %arg2%
-)
+java -jar NoteTaker.jar %arg1% %arg2%
 
 ENDLOCAL
